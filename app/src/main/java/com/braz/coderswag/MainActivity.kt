3998ced56d5473d829e7aaed4a672922e02d878b
Brahim <brahimazaoui.education@gmail.com>
2025-12-28 12:49:09 -0500

    prepare for recycler view adapter

--- app/src/main/java/com/braz/coderswag/MainActivity.kt
+++ app/src/main/java/com/braz/coderswag/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.braz.coderswag
 
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -31,6 +33,9 @@ class MainActivity : AppCompatActivity() {
 
         categoryListView = findViewById(R.id.categoryListView)
         categoryListView.adapter = adapter
+
+
+
 
 
     }
