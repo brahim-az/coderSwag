662056443a6f6f6d211cd97a6afaf510c6dd7fda
Brahim <brahimazaoui.education@gmail.com>
2025-12-27 23:27:26 -0500

    lesson 02: Simple List View

--- app/src/main/java/com/braz/coderswag/MainActivity.kt
+++ app/src/main/java/com/braz/coderswag/MainActivity.kt
@@ -1,20 +1,36 @@
 package com.braz.coderswag
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.braz.coderswag.Model.Category
+import com.braz.coderswag.Services.DataService
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
+     lateinit var categoryListView: ListView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        adapter = ArrayAdapter(this,android.R.layout.simple_list_item_1, DataService.categories)
+
+        categoryListView = findViewById(R.id.categoryListView)
+        categoryListView.adapter = adapter
+
+
     }
 }
--- app/src/main/java/com/braz/coderswag/Model/Category.kt
+++ app/src/main/java/com/braz/coderswag/Model/Category.kt
@@ -1,5 +1,7 @@
 package com.braz.coderswag.Model
 
 class Category(val title: String, val image: String) {
-
+    override fun toString(): String {
+        return title
+    }
 }
