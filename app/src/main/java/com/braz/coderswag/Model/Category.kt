356d86db7af5792088edd61bd4ec55982acf63aa
Brahim <brahimazaoui.education@gmail.com>
2025-12-27 22:31:30 -0500

    lesson 01: Data Structure

---
+++ app/src/main/java/com/braz/coderswag/Model/Category.kt
@@ -0,0 +1,5 @@
+package com.braz.coderswag.Model
+
+class Category(val title: String, val image: String) {
+
+}
---
+++ app/src/main/java/com/braz/coderswag/Model/Product.kt
@@ -0,0 +1,5 @@
+package com.braz.coderswag.Model
+
+class Product(val title: String, val price: String, val image: String) {
+
+}
