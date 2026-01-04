fdefb0a04a0cc4ae6e7430951754a8b508029acd
Brahim <brahimazaoui.education@gmail.com>
2026-01-03 20:21:35 -0500

    lesson 08: Product Recycle Adapter

--- app/src/main/java/com/braz/coderswag/Controller/ProductsActivity.kt
+++ app/src/main/java/com/braz/coderswag/Controller/ProductsActivity.kt
@@ -1,14 +1,23 @@
 package com.braz.coderswag.Controller
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.braz.coderswag.Adapters.ProductsAdapter
 import com.braz.coderswag.R
+import com.braz.coderswag.Services.DataService
 import com.braz.coderswag.Utilities.EXTRA_CATEGORY
 
 class ProductsActivity : AppCompatActivity() {
+
+    lateinit var adapter: ProductsAdapter
+    lateinit var productListView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,6 +30,25 @@ class ProductsActivity : AppCompatActivity() {
         }
 
         val categoryType = intent.getStringExtra(EXTRA_CATEGORY)
-        println(categoryType)
+
+        adapter = ProductsAdapter(this, DataService.getProducts(categoryType))
+
+        var spanCount = 2
+        val orientation = resources.configuration.orientation
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            spanCount = 3
+        }
+
+        val screenSize = resources.configuration.screenWidthDp
+        if (screenSize > 720) {
+            spanCount = 3
+        }
+
+        val layoutManager = GridLayoutManager(this, spanCount)
+        productListView = findViewById(R.id.productsListView)
+        productListView.layoutManager = layoutManager
+        productListView.adapter = adapter
+
+
     }
 }
